ad0bcb4eb9c9b017f7c7dd4ffc7db818d195b5ce
Zhangyifan0016 <z351072259@163.com>
2022-07-07 20:11:17 +0800

    feat(vue.config.js and request.js): 配置跨域以及封装axios请求

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: true,
+  productionSourceMap: false,
+  devServer: {
+    host: 'localhost',
+    port: 9999,
+    https: false,
+    open: true,
+    proxy: {
+      [process.env.VUE_APP_BASE_API]: {
+        target: process.env.VUE_APP_SERVICE_URL,
+        changeOrigin: true,
+        pathRewrite: {
+          ['^' + process.env.VUE_APP_BASE_API]: ''
+        }
+      }
+    }
+  }
 })
