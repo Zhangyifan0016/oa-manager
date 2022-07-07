3ffbebda1afe908235f99cc24f6aca0c4e3fd6ac
Zhangyifan0016 <z351072259@163.com>
2022-07-07 20:23:09 +0800

    feat(api): 封装api users.js

---
+++ src/api/users.js
@@ -0,0 +1,22 @@
+import request from '@/utils/request'
+
+// 用户登录
+const login = () => {
+  return request({ url: '/users/login', method: 'POST' })
+}
+
+// 用户列表
+const getUserList = () => {
+  return request({ url: '/users/list', method: 'GET' })
+}
+
+// 所有用户列表
+const getUserAllList = () => {
+  return request({ url: '/users/all/list', method: 'GET' })
+}
+
+// 用户权限列表
+const getPermissionList = () => {
+  return request({ url: '/users/getPermissionList', method: 'GET' })
+}
+export default { login, getUserList, getUserAllList, getPermissionList }
